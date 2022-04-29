7a20bba624e8603b27a90a3a5b5d0179f9f6605d
Mariah <mariahschock@gmail.com>
2022-04-29 15:04:27 -0700

    creating ball in CSS

--- app.js
+++ app.js
@@ -2,11 +2,32 @@
 const btn1 = document.getElementById('shell1-btn');
 const btn2 = document.getElementById('shell2-btn');
 const btn3 = document.getElementById('shell3-btn');
-// let state
 
+const shell1 = document.getElementById('shell1');
+const shell2 = document.getElementById('shell2');
+const shell3 = document.getElementById('shell3');
+// let state
+let wins = 0;
+let losses = 0;
 // set event listeners 
 btn1.addEventListener('click', () => {
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal');
+
     
+});
+
+btn2.addEventListener('click', () => {
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal');
+});
+
+btn3.addEventListener('click', () => {
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal');
 });
   // get user input
   // use user input to update state 
