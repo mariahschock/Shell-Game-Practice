105d18869f2090ad58ccaa0972638987a1a7a035
Mariah <mariahschock@gmail.com>
2022-04-29 14:56:02 -0700

    image resizing

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // import functions and grab DOM elements
-
+const btn1 = document.getElementById('shell1-btn');
+const btn2 = document.getElementById('shell2-btn');
+const btn3 = document.getElementById('shell3-btn');
 // let state
 
 // set event listeners 
+btn1.addEventListener('click', () => {
+    
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
